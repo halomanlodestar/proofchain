872448fd5e112e26de62576929b78356884e1cfb
halomanlodestar <kunalranarj2005@gmail.com>
2025-03-16 10:57:01 +0530

    refactoring

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,45 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Dashboard from "./pages/dashboard/Dashboard.tsx";
-import { BrowserRouter, Routes, Route } from "react-router";
-import Navbar from "@/components/Navbar/Navbar.tsx";
-import AuthLayout from "@/pages/auth/AuthLayout.tsx";
-import SignInForm from "@/pages/auth/SignInForm.tsx";
-import SignUpForm from "@/pages/auth/SignUpForm.tsx";
-import NotFound from "@/pages/NotFound.tsx";
-import { AuthProvider } from "@/hooks/use-auth.tsx";
-import ProtectedRoutes from "@/components/ProtectedRoutes.tsx";
-import Transactions from "@/pages/transactions";
-import { Landing } from "@/pages/Landing.tsx";
-import NewTransaction from "@/pages/transactions/NewTransaction.tsx";
+import App from "@/pages/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <Navbar />
-        <Routes>
-          <Route path={"/"} index element={<Landing />} />
-
-          {/* Auth */}
-          <Route path={"auth"} element={<AuthLayout />}>
-            <Route path={"signin"} element={<SignInForm />} />
-            <Route path={"signup"} element={<SignUpForm />} />
-          </Route>
-
-          {/* ProtectedRoutes */}
-          <Route element={<ProtectedRoutes />}>
-            <Route path={"/transactions"} element={<Transactions />}>
-              <Route path={"/transactions/new"} element={<NewTransaction />} />
-            </Route>
-            <Route path={"/dashboard"} element={<Dashboard />} />
-          </Route>
-
-          {/* Not Found */}
-          <Route path={"*"} element={<NotFound />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
